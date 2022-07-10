872317e6757a660f4c6f7ebc953e046021b4a03a
Batman <sirati10546@gmail.com>
2022-07-10 12:14:35 +0700

    submit

--- pages/tasks/[...id].jsx
+++ pages/tasks/[...id].jsx
@@ -10,7 +10,6 @@ import Image from "next/image";
 import CodeMirror from "@uiw/react-codemirror";
 import { cpp } from "@codemirror/lang-cpp";
 import { useCompileCodeMutation } from "../../hooks/api/submit/compileSlice";
-import jwtDecode from "jwt-decode";
 import axios from "axios";
 
 const Submit = ({ token, user, questionId, submit }) => {
@@ -46,12 +45,16 @@ const Submit = ({ token, user, questionId, submit }) => {
           <h3>Case {Number(key) + 1}</h3>
           <code className="code-block">
             <div className="input">
-              <h4><div>Input</div></h4>
+              <h4>
+                <div>Input</div>
+              </h4>
               <p>{data.ex_input[key] ? data.ex_input[key] : ""}</p>
             </div>
-            <hr/>
+            <hr />
             <div className="output">
-              <h4><div>Output</div></h4>
+              <h4>
+                <div>Output</div>
+              </h4>
               <p>{data.ex_output[key]}</p>
             </div>
           </code>
@@ -82,88 +85,89 @@ const Submit = ({ token, user, questionId, submit }) => {
   }, [count, reload, router]);
   return (
     <Layout>
-      <Loading className={reload ? "active" : ""}/>
+      <Loading className={reload ? "active" : ""} />
       <div className="submit-wrapper">
         <div className="submit-problem">
           <div className="head">
-            <button
-                type="button"
-                onClick={() => router.back()}
-            >
+            <button type="button" onClick={() => router.back()}>
               BACK
             </button>
             <span className="author">
               {isFetching ? "Loading..." : "by " + data.issuer}
             </span>
           </div>
-          <a href={isFetching ? "" : data.pdfLink} target="_blank" rel="noreferrer">
-            <h1 className="title">
-              {isFetching ? "Loading..." : data.title}
-            </h1>
+          <a
+            href={isFetching ? "" : data.pdfLink}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <h1 className="title">{isFetching ? "Loading..." : data.title}</h1>
           </a>
-          <p className="detail">
-            {isFetching ? "Loading..." : data.detail}
-          </p>
+          <p className="detail">{isFetching ? "Loading..." : data.detail}</p>
           <div className="specification">
             <h2>Specification</h2>
             <div className="table-wrapper">
               <div className="container">
                 <div className="row">
                   <div className="head">Input</div>
-                  <div className="content">{isFetching ? "Loading..." : data.detail_input}</div>
+                  <div className="content">
+                    {isFetching ? "Loading..." : data.detail_input}
+                  </div>
                 </div>
                 <div className="row">
                   <div className="head">Output</div>
-                  <div className="content">{isFetching ? "Loading..." : data.detail_output}</div>
+                  <div className="content">
+                    {isFetching ? "Loading..." : data.detail_output}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          {(example.length !== 0) ? (
+          {example.length !== 0 ? (
             <div className="sample-case">
               <h2>Sample Case</h2>
               {example}
             </div>
-          ) : ""}
+          ) : (
+            ""
+          )}
 
           {data.image ? (
             <div className="image">
               <h2>Image</h2>
               <br />
               <div>
-                <Image
-                  src={data.image}
-                  width={200}
-                  height={200}
-                  alt="image"
-                />
+                <Image src={data.image} width={200} height={200} alt="image" />
               </div>
             </div>
-          ) : ""}
-          {(data.note) ? (
+          ) : (
+            ""
+          )}
+          {data.note ? (
             <div className="hint">
               <h2>Hint : </h2>
               <p>{data.note}</p>
             </div>
-          ) : ""}
+          ) : (
+            ""
+          )}
         </div>
         <div className="submit-code">
-          {(submit
-            ? submit.status === true
-              ? <div className="ribbon absolute green">Complete</div>
-              : <div className="ribbon absolute orange">In Progress...</div>
-            : <div className="ribbon absolute gray">Incomplete</div>
+          {submit ? (
+            submit.status === true ? (
+              <div className="ribbon absolute green">Complete</div>
+            ) : (
+              <div className="ribbon absolute orange">In Progress...</div>
+            )
+          ) : (
+            <div className="ribbon absolute gray">Incomplete</div>
           )}
           <div className="header mt-[35px]">
             <div className="result">
               <h1>RESULT</h1>
               <p
                 className={`${
-                  submit
-                    ? submit.status === true
-                      ? "success"
-                      : "failed"
-                    : ""
+                  submit ? (submit.status === true ? "success" : "failed") : ""
                 }`}
               >
                 {submit.result ? submit.result : "-"}
@@ -171,16 +175,14 @@ const Submit = ({ token, user, questionId, submit }) => {
             </div>
             <div className="finish">
               <h1>FINISHED</h1>
-              <p>
-                {isFetching ? "Loading" : data.finished}
-              </p>
+              <p>{isFetching ? "Loading" : data.finished}</p>
             </div>
           </div>
           <form className="code" onSubmit={handleSubmit}>
             <div className="code-editor">
               <CodeMirror
                 value={submit.sourceCode ? submit.sourceCode : ""}
-                extensions={[cpp()]}
+                extensions={[cpp(), globalCppCompletions]}
                 theme="dark"
                 className="whitespace-pre monospace bg-[#2A303C]"
                 placeholder={"Write your program..."}
@@ -189,18 +191,18 @@ const Submit = ({ token, user, questionId, submit }) => {
                   setSourceCode(value);
                 }}
                 aria-label="code"
+                autoCorrect="true"
               />
             </div>
+
             <div className="code-submit">
-              <button type="submit">
-                SUBMIT
-              </button>
+              <button type="submit">SUBMIT</button>
             </div>
           </form>
         </div>
       </div>
     </Layout>
-  )
+  );
 };
 export const getServerSideProps = async (context) => {
   const isAuth = getCookie("token", context);
